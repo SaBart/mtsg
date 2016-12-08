ac0f082071ba945366cfb2e265b37a7dee087c28
SaBart <samuell.bartos@gmail.com>
2016-12-08 21:51:32 +0100

    preprocessing

--- load_forecast/src/models/test.py
+++ load_forecast/src/models/test.py
@@ -4,53 +4,54 @@ Created on 4 Dec 2016
 @author: SABA
 '''
 
-import os.path
-import sys
-import csv
-import numpy as np
-import sklearn as skl
+import pandas
+import numpy
+import sklearn
 import matplotlib.pyplot as mp
 
 from keras.models import Sequential
 from keras.layers.core import Dense
 
-if __name__ == '__main__':
-	
-	seed=0
-	np.random.seed(seed)
+# loading load data
 
-	# loading data
-	print('Loading data')
-	
-	load_csv=r'C:\Users\SABA\Google Drive\mtsg\code\load_forecast\data\load.csv'
-	
-	
-	with open(load_csv, 'r') as csvfile:
-		load_raw=np.array(list(csv.reader(csvfile))).astype('float')
-		
-	load_h=np.reshape(load_raw,(-1,60),order='C').sum(axis=1) # hourly aggregation
-	load_dh=np.reshape(load_h,(365,-1),order='C')
-	
-	X=np.arange(0,np.size(load_dh, axis=0))
-	
-	
-	#mp.plot(np.arange(1,25),load_dh[1])
-	#mp.show()
-	
-	#create model
-	print('creating model')
-	
-	model = Sequential()
-	model.add(Dense(100, input_dim=1, init='uniform', activation='relu'))
-	model.add(Dense(24, init='uniform', activation='sigmoid'))
-	model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
-	
-	# training
-	print('Training')
-	
-	model.fit(X, load_dh, batch_size=10, nb_epoch=10000, verbose=2, validation_split=0.3, shuffle=True)
-	
-	scores = model.evaluate(X, load_dh)
-	print("%s: %.2f%%" % (model.metrics_names[1], scores[1]))
-	
-	
+load_raw =pandas.read_csv(r'C:\Users\SABA\Google Drive\mtsg\data\homeB-all\homeB-power\2012-Apr-15.csv',header=None,sep=",",usecols=[0,1], names=['timestamp','load'],index_col=[0]) # load loads
+
+# add previous values for missing timestamps
+# maybe predict them from a couple of previus values?
+start=load_raw.index.min() # first value of new index
+idx=pandas.Index(numpy.arange(start,start+60*60*24)) # timestamps for the whole day, end at 23:59:59
+load_filled=load_raw.reindex(idx, method='nearest') # fill missing with previous values
+
+# hourly aggregation
+
+load_hours=load_filled.as_matrix(columns=['load']).reshape(-1,3600).sum(axis=1)
+
+mp.plot(numpy.arange(1,25),load_hours)
+mp.show()
+
+ 
+#load_dh=numpy.reshape(load_h,(365,-1),order='C')
+
+X=numpy.arange(0,numpy.size(load_dh, axis=0))
+
+
+seed=0
+np.random.seed(seed)
+
+
+#create model
+print('creating model')
+
+model = Sequential()
+model.add(Dense(100, input_dim=1, init='uniform', activation='relu'))
+model.add(Dense(24, init='uniform', activation='sigmoid'))
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
+
+# training
+print('Training')
+
+model.fit(X, load_dh, batch_size=10, nb_epoch=10000, verbose=2, validation_split=0.3, shuffle=True)
+
+scores = model.evaluate(X, load_dh)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]))
+
