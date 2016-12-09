d051f4c96ae4941034b192523e059bc262aa0b8d
SaBart <samuell.bartos@gmail.com>
2016-12-09 18:32:12 +0100

    processing environmental + dates

--- load_forecast/src/models/test.py
+++ load_forecast/src/models/test.py
@@ -3,7 +3,7 @@ Created on 4 Dec 2016
 
 @author: SABA
 '''
-
+import os
 import pandas
 import numpy
 import sklearn
@@ -11,26 +11,70 @@ import matplotlib.pyplot as mp
 
 from keras.models import Sequential
 from keras.layers.core import Dense
+from numpy.distutils.conv_template import file
+from pandas.io.tests.parser import parse_dates
 
-# loading load data
 
-load_raw =pandas.read_csv(r'C:\Users\SABA\Google Drive\mtsg\data\homeB-all\homeB-power\2012-Apr-15.csv',header=None,sep=",",usecols=[0,1], names=['timestamp','load'],index_col=[0]) # load loads
+# PARAMETERS
 
-# add previous values for missing timestamps
-# maybe predict them from a couple of previus values?
-start=load_raw.index.min() # first value of new index
-idx=pandas.Index(numpy.arange(start,start+60*60*24)) # timestamps for the whole day, end at 23:59:59
-load_filled=load_raw.reindex(idx, method='nearest') # fill missing with previous values
+time_ints=24 # number of time intervals
+cols=numpy.arange(1,1+time_ints).tolist() # column names
 
-# hourly aggregation
 
-load_hours=load_filled.as_matrix(columns=['load']).reshape(-1,3600).sum(axis=1)
+# LOADING ENVIRONMENTAL DATA
+dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-environmental/'
 
-mp.plot(numpy.arange(1,25),load_hours)
+temp_in = pandas.DataFrame(columns=cols) # inside temperature
+temp_out = pandas.DataFrame(columns=cols) # outside temperature
+hum_in = pandas.DataFrame(columns=cols) # inside humidity
+hum_out = pandas.DataFrame(columns=cols) # outside humidity
+
+for file in os.listdir(dir_data):
+	if file.endswith(".csv"):
+        env_raw =pandas.read_csv(r'C:\Users\SABA\Google Drive\mtsg\data\homeB-all\homeB-power\2012-Apr-15.csv',header=None,sep=",",usecols=[0,1,2,3,4], names=['timestamp','temp_in','temp_out','hum_in','hum_out'],index_col=[0]) # load loads
+		# add previous values for missing timestamps
+		# maybe predict them from a couple of previus values?
+		start=env_raw.index.min() # first value of new index
+		idx=pandas.Index(numpy.arange(start=start,stop=start+60*60*24,step=300)) # timestamps for the whole day, end at 23:59:59
+		env_full=env_raw.reindex(idx, method='nearest') # fill missing with nearest values (predict them?)
+		temp_in_hrs=load_full.as_matrix(columns=['temp_in']).reshape(-1,load_full.shape[0]//time_ints).average(axis=1) # averaging inside temperature
+		temp_out_hrs=load_full.as_matrix(columns=['temp_out']).reshape(-1,load_full.shape[0]//time_ints).average(axis=1) # averaging outside temperature
+		hum_in_hrs=load_full.as_matrix(columns=['hum_in']).reshape(-1,load_full.shape[0]//time_ints).average(axis=1) # averaging inside humidity
+		hum_out_hrs=load_full.as_matrix(columns=['hum_out']).reshape(-1,load_full.shape[0]//time_ints).average(axis=1) # averaging outside humidity
+		temp_in.loc[temp_in.shape[0]]=temp_in_hrs
+		temp_out.loc[temp_out.shape[0]]=temp_out_hrs
+		hum_in.loc[hum_in.shape[0]]=hum_in_hrs
+		hum_out.loc[hum_out.shape[0]]=hum_out_hrs
+
+
+
+mp.plot(loads.loc[80])
+mp.show()
+
+
+
+# LOADING LOAD DATA
+dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeC-all/homeC-power/'
+loads = pandas.DataFrame(columns=['day'] + cols)
+
+for file in os.listdir(dir_data):
+	if file.endswith(".csv"):
+        load_raw =pandas.read_csv(dir_data+file,header=None,sep=",",usecols=[0,1], parse_dates=[0], names=['timestamp','load'],index_col=[0]) # load loads
+		# add previous values for missing timestamps
+		# maybe predict them from a couple of previus values?
+		start=load_raw.index.min() # first value of new index
+		idx=pandas.Index(numpy.arange(start,start+60*60*24)) # timestamps for the whole day, end at 23:59:59
+		load_full=load_raw.reindex(idx, method='nearest') # fill missing with nearest values (predict them?)
+		load_hrs=load_full.as_matrix(columns=['load']).reshape(-1,load_full.shape[0]//time_ints).sum(axis=1) # aggregation for time intervals
+		loads.loc[loads.shape[0]]=[pandas.to_datetime(start,unit='s')]+load_hrs.tolist()
+
+loads.set_index(pandas.DatetimeIndex(loads['day']), inplace=True)
+
+mp.plot(loads.loc[80])
 mp.show()
 
  
-#load_dh=numpy.reshape(load_h,(365,-1),order='C')
+load_dh=numpy.reshape(load_h,(365,-1),order='C')
 
 X=numpy.arange(0,numpy.size(load_dh, axis=0))
 
@@ -54,4 +98,3 @@ model.fit(X, load_dh, batch_size=10, nb_epoch=10000, verbose=2, validation_split
 
 scores = model.evaluate(X, load_dh)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]))
-
