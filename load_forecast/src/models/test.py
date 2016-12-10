4b1e414fea3fb1af345b4a74ef82daa6073ee156
SaBart <samuell.bartos@gmail.com>
2016-12-10 18:18:49 +0100

    reworked load loading

--- load_forecast/src/models/test.py
+++ load_forecast/src/models/test.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as mp
 
 from keras.models import Sequential
 from keras.layers.core import Dense
-from numpy.distutils.conv_template import file
-from pandas.io.tests.parser import parse_dates
+#from numpy.distutils.conv_template import file
+#from pandas.io.tests.parser import parse_dates
 
 
 # PARAMETERS
@@ -51,28 +51,91 @@ for file in os.listdir(dir_data):
 mp.plot(loads.loc[80])
 mp.show()
 
+# MISSING DATA HISTOGRAM 
+
+dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-power/'
+#loads_all = pandas.DataFrame(columns=['time','load'])
+loads_list=[]
+
+for file in os.listdir(dir_data):
+	if file.endswith(".csv"):
+		load_raw =pandas.read_csv(dir_data+file,header=None,sep=",",usecols=[0,1], parse_dates=True, names=['time','load'],index_col=[0]) # load loads
+		loads_list.append(load_raw)
+		
+loads_all=pandas.concat(loads_list)		
+
+
+
 
 
 # LOADING LOAD DATA
-dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeC-all/homeC-power/'
+dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-power/'
+loads = pandas.DataFrame()
+
+for file in os.listdir(dir_data):
+	if file.endswith(".csv"):
+		load_raw =pandas.read_csv(dir_data+file,header=None,sep=",",usecols=[0,1], parse_dates=True, date_parser=(lambda x:pandas.to_datetime(x,unit='s')), names=['time','load'],index_col=[0]) # load loads
+		if load_raw.shape[0]/(60*60*24)<0.90: # discard file with more than 5% missing data
+			continue
+		# add previous values for missing timestamps
+		# maybe predict them from a couple of previus values?
+		start=load_raw.index.min() # first value of new index
+		idx=pandas.date_range(start=start,periods=86400,freq='1S') # timestamps for the whole day, end at 23:59:59
+		load_full=load_raw.reindex(idx, method='nearest') # fill missing with nearest values (predict them?)
+		load_hrs=load_full.resample('H').sum() # hourly aggregation of loads
+		if (loads.shape[0]<10):
+			load_raw.plot()
+			load_hrs.plot()
+		# build new entry for current day
+		load_hrs['date']=pandas.DatetimeIndex(load_hrs.index).date # new index
+		load_hrs['time']=pandas.DatetimeIndex(load_hrs.index).time # new columns
+		loads=loads.append(load_hrs.pivot(index='date', columns='time', values='load')) # pivot & append new entry
+
+loads.ix['2012-06-04'].plot(y='load')
+
+mp.plot(numpy.arange(24),loads.ix[19].values[1:])
+mp.plot(numpy.arange(24),load_hrs.tolist())
+mp.show() 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-power/'
 loads = pandas.DataFrame(columns=['day'] + cols)
 
 for file in os.listdir(dir_data):
 	if file.endswith(".csv"):
-        load_raw =pandas.read_csv(dir_data+file,header=None,sep=",",usecols=[0,1], parse_dates=[0], names=['timestamp','load'],index_col=[0]) # load loads
+		load_raw =pandas.read_csv(dir_data+file,header=None,sep=",",usecols=[0,1], parse_dates=True, date_parser=(lambda x:pandas.to_datetime(x,unit='s')), names=['time','load'],index_col=[0]) # load loads
+		if load_raw.shape[0]/(60*60*24)<0.90: # discard file with more than 5% missing data
+			continue
 		# add previous values for missing timestamps
 		# maybe predict them from a couple of previus values?
 		start=load_raw.index.min() # first value of new index
-		idx=pandas.Index(numpy.arange(start,start+60*60*24)) # timestamps for the whole day, end at 23:59:59
+		idx=pandas.date_range(start=start,periods=86400,freq='1S') # timestamps for the whole day, end at 23:59:59
+		#idx=pandas.Index(numpy.arange(start,start+60*60*24)) # timestamps for the whole day, end at 23:59:59
 		load_full=load_raw.reindex(idx, method='nearest') # fill missing with nearest values (predict them?)
-		load_hrs=load_full.as_matrix(columns=['load']).reshape(-1,load_full.shape[0]//time_ints).sum(axis=1) # aggregation for time intervals
-		loads.loc[loads.shape[0]]=[pandas.to_datetime(start,unit='s')]+load_hrs.tolist()
+		load_full.resample('H').sum() # aggregate hours
+		#load_hrs=load_full.as_matrix(columns=['load']).reshape(-1,load_full.shape[0]//time_ints).sum(axis=1) # aggregation for time intervals
+		pandas.concat(loads,)
+		loads.loc[loads.shape[0]]=[start]+load_hrs.tolist()
 
-loads.set_index(pandas.DatetimeIndex(loads['day']), inplace=True)
+loads.set_index('day', inplace=True)
 
-mp.plot(loads.loc[80])
-mp.show()
-
+ 
+ 
+ 
+ 
  
 load_dh=numpy.reshape(load_h,(365,-1),order='C')
 
