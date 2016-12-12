147fc245d32f1c264f83bef261ab288883d614a7
SaBart <samuell.bartos@gmail.com>
2016-12-12 23:28:04 +0100

    data lag

--- load_forecast/src/models/test.py
+++ load_forecast/src/models/test.py
@@ -31,7 +31,7 @@ hum_out = pandas.DataFrame(columns=cols) # outside humidity
 
 for file in os.listdir(dir_data):
 	if file.endswith(".csv"):
-        env_raw =pandas.read_csv(r'C:\Users\SABA\Google Drive\mtsg\data\homeB-all\homeB-power\2012-Apr-15.csv',header=None,sep=",",usecols=[0,1,2,3,4], names=['timestamp','temp_in','temp_out','hum_in','hum_out'],index_col=[0]) # load loads
+		env_raw =pandas.read_csv(r'C:\Users\SABA\Google Drive\mtsg\data\homeB-all\homeB-power\2012-Apr-15.csv',header=None,sep=",",usecols=[0,1,2,3,4], names=['timestamp','temp_in','temp_out','hum_in','hum_out'],index_col=[0]) # load loads
 		# add previous values for missing timestamps
 		# maybe predict them from a couple of previus values?
 		start=env_raw.index.min() # first value of new index
@@ -65,9 +65,6 @@ for file in os.listdir(dir_data):
 loads_all=pandas.concat(loads_list)		
 
 
-
-
-
 # LOADING LOAD DATA
 dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-power/'
 loads = pandas.DataFrame()
@@ -83,9 +80,6 @@ for file in os.listdir(dir_data):
 		idx=pandas.date_range(start=start,periods=86400,freq='1S') # timestamps for the whole day, end at 23:59:59
 		load_full=load_raw.reindex(idx, method='nearest') # fill missing with nearest values (predict them?)
 		load_hrs=load_full.resample('H').sum() # hourly aggregation of loads
-		if (loads.shape[0]<10):
-			load_raw.plot()
-			load_hrs.plot()
 		# build new entry for current day
 		load_hrs['date']=pandas.DatetimeIndex(load_hrs.index).date # new index
 		load_hrs['time']=pandas.DatetimeIndex(load_hrs.index).time # new columns
@@ -96,21 +90,72 @@ loads.ix['2012-06-04'].plot(y='load')
 mp.plot(numpy.arange(24),loads.ix[19].values[1:])
 mp.plot(numpy.arange(24),load_hrs.tolist())
 mp.show() 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+# LOADING GENERATOR DATA
+
+def load_data(path='C:/Users/SABA/Google Drive/mtsg/code/generator/out/Electricity_Profile.csv',lag=1):
+	# loading
+	load_raw =pandas.read_csv(path,header=None,sep=",",usecols=[0], names=['load']) # load loads
+	load=load_raw.groupby(load_raw.index//60).sum() # hourly aggregation
+	load['hour']=pandas.Series(numpy.concatenate([numpy.arange(1,25)]*365)) # new column for pivoting
+	load['day']=pandas.Series(numpy.repeat(numpy.arange(1,366), repeats=24)) # new column for pivoting
+	load=load.pivot(index='day',columns='hour',values='load') # pivoting
+	return load
+
+
+
+
+
+
+def lag_data(data,lag=1):
+	data_lagged={} # lagged dataframes for merging
+	for i in range(0,lag+1): # for each time step
+		data_lagged[i-lag]=data.shift(-i) # add lagged dataframe
+	res=pandas.concat(data_lagged.values(),axis=1,join='inner',keys=data_lagged.keys()) # merge lagged dataframes	
+	return res.dropna()
+
+
+
+def cut_data(data,steps=1):
+	new_dict={}
+	tuples=[] # multiIndex
+	for col in data:
+		new_dict[col]=data[col] # add old column
+		for i in range(1,steps+1):
+			new_dict['%s_lag%d' %(col,i)]=data[col].shift(i) # add shifted column
+			tuples.append((i,col)) # new multiIndex entry 
+	res=pandas.DataFrame(new_dict,index=data.index) 
+	return res.dropna(axis=0)
+	
+	
+	
+	
+df1 = pandas.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],'B': ['B0', 'B1', 'B2', 'B3'],'C': ['C0', 'C1', 'C2', 'C3'],'D': ['D0', 'D1', 'D2', 'D3']}, index=[0, 1, 2, 3])
+
+df2 = pandas.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],'B': ['B4', 'B5', 'B6', 'B7'],'C': ['C4', 'C5', 'C6', 'C7'],'D': ['D4', 'D5', 'D6', 'D7']},index=[4, 5, 6, 7])
+
+
+
+
+
+	
+	for col in data:
+		new_dict[col]=data[col] # add old column
+		for i in range(1,steps+1):
+			new_dict['%s_lag%d' %(col,i)]=data[col].shift(i) # add shifted column 
+	res=pandas.DataFrame(new_dict,index=data.index) 
+	return res.dropna(axis=0)
+
+
+
+
+load.plot(kind='bar')
+
+
+
+
 dir_data='C:/Users/SABA/Google Drive/mtsg/data/homeB-all/homeB-power/'
 loads = pandas.DataFrame(columns=['day'] + cols)
 
