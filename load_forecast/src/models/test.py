903d8551a6d48c71695ec691de3d81f424b1b754
SaBart <samuell.bartos@gmail.com>
2016-12-14 23:51:20 +0100

    grid search

--- load_forecast/src/models/test.py
+++ load_forecast/src/models/test.py
@@ -8,8 +8,6 @@ import numpy
 import sklearn
 import pandas
 import matplotlib.pyplot as mp
-from keras.models import Sequential
-from keras.layers.core import Dense
 #from numpy.distutils.conv_template import file
 #from pandas.io.tests.parser import parse_dates
 
@@ -110,32 +108,117 @@ def lag_data(data,lag=1):
 	return res.dropna()
 
 # separates data into training & testing sets & converts dataframes to numpy matrices 
-def format_data(path='C:/Users/SABA/Google Drive/mtsg/code/generator/out/Electricity_Profile.csv', l=1):
-	from sklearn.model_selection import train_test_split
-	data=lag_data(load_data(path),l)
-	train, test =train_test_split(data, test_size=0.2)
-	X_train=train.select(lambda x:x[0] not in [0], axis=1)
-	Y_train=train[0]
-	X_test=test.select(lambda x:x[0] not in [0], axis=1)
-	Y_test=test[0]
+def format_data(path='C:/Users/SABA/Google Drive/mtsg/code/generator/out/Electricity_Profile.csv', lag=1, test_size=0.2):
+	data=lag_data(load_data(path),lag)
+	train, test =split_train_test(data, test_size)
+	X_train,Y_train=split_X_Y(train)
+	X_test,Y_test=split_X_Y(test)
 	return X_train.as_matrix(), Y_train.as_matrix(), X_test.as_matrix(), Y_test.as_matrix()
 
-# defines simple NN model
-def model(X_train, Y_train, X_test, Y_test):
+# split data into X & Y
+def split_X_Y(data):
+	X=data.select(lambda x:x[0] not in [0], axis=1)
+	Y=data[0]
+	return X, Y
+
+# split data into train & test sets
+def split_train_test(data, test_size=0.2):
+	from sklearn.model_selection import train_test_split
+	train, test =train_test_split(data, test_size=test_size)
+	return train,test
+	
+# create & train basic NN model
+def create_model(nb_in=24, nb_out=24, nb_hidden=50, nb_epoch=200, batch_size=1, activation='relu', loss='mean_squared_error', optimizer='adam'):
+	from keras.models import Sequential
+	from keras.layers.core import Dense
 	model = Sequential() # FFN
-	model.add(Dense(25, input_dim=X_train.shape[1],activation='relu')) # input & hidden layers
+	model.add(Dense(nb_hidden, input_dim=nb_in,activation=activation)) # input & hidden layers
 	#model.add(Dropout({{uniform(0, 1)}})) # randomly set a number of inputs to 0 to prevent overfitting
-	model.add(Dense(24, activation='sigmoid')) # output layer
-	model.compile(loss='mean_squared_error', optimizer='adam') # assemble network
-	model.fit(X_train, Y_train, batch_size=100, nb_epoch=200, verbose=2, validation_data=(X_test, Y_test))
-	score = model.evaluate(X_test, Y_test, verbose=0)
-	print('Score:', score)
+	model.add(Dense(nb_out)) # output layer
+	model.compile(loss=loss, optimizer=optimizer) # assemble network	
+	return model
 
-X_train,Y_train,X_test,Y_test=format_data('C:/Users/SABA/Google Drive/mtsg/code/generator/out/Electricity_Profile.csv')
-model(X_train,Y_train,X_test,Y_test)
+from sklearn.metrics import mean_squared_error, make_scorer
+mse = make_scorer(mean_squared_error, multioutput='uniform_average')
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+seed=0 # fix seed for reprodicibility
+numpy.random.seed(seed)
+path='C:/Users/SABA/Google Drive/mtsg/code/generator/out/Electricity_Profile.csv' # data path
+X,Y=split_X_Y(lag_data(load_data(path),lag=1)) # prepare data
+model = KerasClassifier(build_fn=create_model)
+
+#nb_hidden=[10,20,30,40,50,60,70,80,90,100] # domain for number of hidden neurons
+nb_in=[X.shape[1]]
+nb_out=[Y.shape[1]]
+nb_hidden=[10,20,30]
+param_grid={'nb_hidden':nb_hidden} # grid parameter space
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, scoring='r2')
+grid_result = grid.fit(X.as_matrix(), Y.as_matrix())
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 
-# simple NN optimisation
+
+
+model=create_model(X_train.shape[1],Y_train.shape[1])
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=2)
+data_pred=model.predict(X_test)
+
+mp.plot(data_pred[0])
+mp.plot(Y_test[0])
+mp.show()
+
+
+
+# Use scikit-learn to grid search the number of neurons
+import numpy
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.constraints import maxnorm
+# Function to create model, required for KerasClassifier
+def create_model(neurons=1):
+	# create model
+	model = Sequential()
+	model.add(Dense(neurons, input_dim=8, init='uniform', activation='linear', W_constraint=maxnorm(4)))
+	model.add(Dropout(0.2))
+	model.add(Dense(1, init='uniform', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load dataset
+dataset = numpy.loadtxt('C:/Users/SABA/Google Drive/mtsg/code/load_forecast/src/models/pima-indians-diabetes.csv', delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = KerasClassifier(build_fn=create_model, nb_epoch=100, batch_size=10, verbose=0)
+# define the grid search parameters
+neurons = [1, 5, 10, 15, 20, 25, 30]
+param_grid = dict(neurons=neurons)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+grid_result = grid.fit(X, Y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
 
 
 from hyperopt import Trials, STATUS_OK, tpe
@@ -256,3 +339,63 @@ model.fit(X, load_dh, batch_size=10, nb_epoch=10000, verbose=2, validation_split
 
 scores = model.evaluate(X, load_dh)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]))
+
+
+
+# Multilayer Perceptron to Predict International Airline Passengers (t+1, given t, t-1, t-2)
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
+dataframe = pandas.read_csv('C:/Users/SABA/Google Drive/mtsg/code/load_forecast/src/models/international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+# reshape dataset
+look_back = 10
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+# create and fit Multilayer Perceptron model
+model = Sequential()
+model.add(Dense(8, input_dim=look_back, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
+# Estimate model performance
+trainScore = model.evaluate(trainX, trainY, verbose=0)
+print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore, math.sqrt(trainScore)))
+testScore = model.evaluate(testX, testY, verbose=0)
+print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore, math.sqrt(testScore)))
+# generate predictions for training
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+mp.plot(dataset)
+mp.plot(trainPredictPlot)
+mp.plot(testPredictPlot)
+mp.show()
