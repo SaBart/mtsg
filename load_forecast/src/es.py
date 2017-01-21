b2b55ba2437c94845529ef0196e41968a77163cf
SaBart <samuell.bartos@gmail.com>
2017-01-21 17:52:21 +0100

    R exponential smoothing

--- load_forecast/src/es.py
+++ load_forecast/src/es.py
@@ -5,11 +5,93 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import dataprep as dp
 import patsy
-import pycast
-from pycast.methods.exponentialsmoothing import HoltWintersMethod
 from sklearn.metrics import r2_score
+
+
+# computes the unitial trend for all hours
+def initial_trend(data, slen):
+    return ((data-data.shift(slen))[slen:slen*2]).sum(axis=0)/(slen**2)
+
+def my_initial_seasonal_components(series, slen):
+    seasonals = {}
+    season_averages = []
+    n_seasons = int(len(series)/slen)
+    # compute season averages
+    for j in range(n_seasons):
+        season_averages.append(sum(series[slen*j:slen*j+slen])/float(slen))
+    # compute initial values
+    for i in range(slen):
+        sum_of_vals_over_avg = 0.0
+        for j in range(n_seasons):
+            sum_of_vals_over_avg += series[slen*j+i]-season_averages[j]
+        seasonals[i] = sum_of_vals_over_avg/n_seasons
+    return seasonals
+
+def initial_seasonal_components(series, slen):
+    seasonals = {}
+    season_averages = []
+    n_seasons = int(len(series)/slen)
+    # compute season averages
+    for j in range(n_seasons):
+        season_averages.append(sum(series[slen*j:slen*j+slen])/float(slen))
+    # compute initial values
+    for i in range(slen):
+        sum_of_vals_over_avg = 0.0
+        for j in range(n_seasons):
+            sum_of_vals_over_avg += series[slen*j+i]-season_averages[j]
+        seasonals[i] = sum_of_vals_over_avg/n_seasons
+    return seasonals
+
+def triple_exponential_smoothing(series, slen, alpha, beta, gamma, n_preds):
+    result = []
+    seasonals = initial_seasonal_components(series, slen)
+    for i in range(len(series)+n_preds):
+        if i == 0: # initial values
+            smooth = series[0]
+            trend = initial_trend(series, slen)
+            result.append(series[0])
+            continue
+        if i >= len(series): # we are forecasting
+            m = i - len(series) + 1
+            result.append((smooth + m*trend) + seasonals[i%slen])
+        else:
+            val = series[i]
+            last_smooth, smooth = smooth, alpha*(val-seasonals[i%slen]) + (1-alpha)*(smooth+trend)
+            trend = beta * (smooth-last_smooth) + (1-beta)*trend
+            seasonals[i%slen] = gamma*(val-smooth) + (1-gamma)*seasonals[i%slen]
+            result.append(smooth+trend+seasonals[i%slen])
+    return results
 
 np.random.seed(0) # fix seed for reprodicibility
 path='C:/Users/SABA/Google Drive/mtsg/data/household_power_consumption.csv' # data path
 load_raw=dp.load(path) # load data
 targets=load_raw.apply(axis=1,func=(lambda x: np.nan if (x.isnull().sum()>0) else x.mean())).unstack() # custom sum function where any Nan in arguments gives Nan as result
+
+
+
+
+
+
+
+
+
+
+series = [30,21,29,31,40,48,53,47,37,39,31,29,17,9,20,24,27,35,41,38,
+          27,31,27,26,21,13,21,18,33,35,40,36,22,24,21,20,17,14,17,19,
+          26,29,40,31,20,24,18,26,17,9,17,21,28,32,46,33,23,28,22,27,
+          18,8,17,21,31,34,44,38,31,30,26,32]
+
+
+def init_trend(series, slen):
+    sum = 0.0
+    for i in range(slen):
+        sum += float(series[i+slen] - series[i])
+    return sum / slen**2
+
+init_trend(series, 12) # -0.7847222222222222
+initial_trend(pd.DataFrame(series),12)
+
+
+
+
+
